36d015cec0f2afd425bd94e2ae9f06d2d93bbfb3
Grant-92 <35877359+Grant-92@users.noreply.github.com>
2019-03-08 13:33:14 +0000

    initial

---
+++ app/src/main/java/com/example/ndca/HomeFragment.java
@@ -0,0 +1,36 @@
+package com.example.ndca;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import com.lalongooo.Rings;
+
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+        Rings rings = view.findViewById(R.id.rings);
+
+        // Inflate the layout for this fragment
+
+        return view;
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/ndca/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.ndca
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //bottom nav clicks
+        design_navigation_view.setOnNavigationItemSelectedListener { item ->
+
+            var message = ""
+
+            when (item.itemId) {
+                R.id.action_home -> {
+                    replaceFragment(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.action_Diet -> {
+                    replaceFragment(DietFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_settings -> {
+                    replaceFragment(SettingsFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+            }
+            Toast.makeText(
+                this,
+                "$message clicked",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            return@setOnNavigationItemSelectedListener true
+        }
+        false
+    }
+
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.rel_layout, fragment)
+        fragmentTransaction.commit()
+
+    }
+
+
+}
