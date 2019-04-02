69378db4ce95bfacd10eb937ea3e1d1df5a7495c
Grant-92 <35877359+grant-92@users.noreply.github.com>
2019-04-02 13:05:10 +0100

    sign-in implemented

---
+++ app/src/main/java/com/example/ndca/LoginActivity.java
@@ -0,0 +1,88 @@
+package com.example.ndca;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
+    private EditText mEmail, mPassword;
+    private Button logIn, signUp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mEmail = findViewById(R.id.email_edit_text);
+        mPassword = findViewById(R.id.pass_edit_text);
+
+        logIn = findViewById(R.id.sign_in_btn);
+        signUp = findViewById(R.id.reg_btn);
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        //TODO check if user is signed in and act accordingly
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.sign_in_btn) {
+
+            String email = mEmail.getText().toString();
+            String password = mPassword.getText().toString();
+
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("TAG", "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+
+                                //TODO might be better  to pass another value over UID
+                                String id = user.getUid();
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                //TODO retreive UID in HomeFragment
+                                intent.putExtra("id", id);
+                                startActivity(intent);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("TAG", "signInWithEmail:failure", task.getException());
+                                Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+                            // ...
+                        }
+
+                    });
+
+
+        } else if (v.getId() == R.id.reg_btn) {
+
+        }
+    }
+}
